8ad6ef802892b8be6c57f5697680642fe098cb92
Thomas Munoz <thomas.fmunoz@gmail.com>
2016-09-20 00:49:50 -0300

    Update secante.m

--- Matlab/Funciones/secante.m
+++ Matlab/Funciones/secante.m
@@ -1,6 +1,6 @@
 function r=fposicion(fnc,x0,x1,e)
 	while true
-		xnew=(x0*fnc(x1)-x1*nc(x0))/fnc(x1)-fnc(x0)
+		xnew=(x0*fnc(x1)-x1*nc(x0))/(fnc(x1)-fnc(x0))
 		if abs(xnew-x1) < e
 			r=xnew
 			return
