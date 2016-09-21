8ff1fb87da4f2d78e1b90c9a9ece3db35d467342
diego.vilches4 <diego.vilchesa@mail.udp.cl>
2016-09-21 00:05:15 -0300

    newton has been defeated

--- Matlab/Ejercicios/Ej 2/newton.m
+++ Matlab/Ejercicios/Ej 2/newton.m
@@ -7,12 +7,13 @@ function nt = newton(fnc,x0,er)
  ab=abs(x1-x0)
  while ab>er
      x0=x1;
-     f=subs(fnc,x0);
-     df=subs(dfun,x0);
-     x1=x0-f/df;
-     ab=abs(x1-x0)
-     c=c+1
+     f=double(subs(fnc,x0));
+     df=double(subs(dfun,x0));
+     x1=double(x0-f/df);
+     ab=double(abs(x1-x0));
+     c=c+1;
  end 
  nt=x1
+ c
 end
 
