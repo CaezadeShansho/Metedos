fccea717cb33dc7e8d1c9216a03d099edfdd789c
diego.vilches4 <diego.vilchesa@mail.udp.cl>
2016-09-29 21:13:46 -0300

    pregunta 2 redactada, faltan detalles menores

--- Matlab/Ejercicios/Ej 6/schroder.m
+++ Matlab/Ejercicios/Ej 6/schroder.m
@@ -4,18 +4,19 @@ function sf=schroder(fnc,x0,er)
     f=double(subs(fnc,x0));
     df=double(subs(dfnc,x0));
     ddf=double(subs(ddfnc,x0));
-    xn=double((x0-f)/(df^2-f*ddf));
+    xn=double(x0-f*df/(df^2-f*ddf));
     ab=double(abs(xn-x0)/abs(xn));
-    c=1
+    c=1;
     while ab>er
         x0=xn;
         f=double(subs(fnc,x0));
         df=double(subs(dfnc,x0));
         ddf=double(subs(ddfnc,x0));
-        xn=double((x0-f)/(df^2-f*ddf));
+        xn=double(x0-f*df/(df^2-f*ddf));
         ab=double(abs(xn-x0)/abs(xn));
-        c=c+1
+        c=c+1;
     end
+    c
     sf=xn
 end
 
--- Matlab/Ejercicios/Ej 6/whittaker.m
+++ Matlab/Ejercicios/Ej 6/whittaker.m
@@ -7,7 +7,7 @@ function wf=whittaker(fnc,x0,er)
     lf=double(f*ddf/df^2);
     xn=double(x0-f*(2-lf)/(2*df));
     ab=double(abs(xn-x0)/abs(xn));
-    c=1
+    c=1;
     while ab>er
     	f=double(subs(fnc,x0));
     	df=double(subs(dfnc,x0));
@@ -15,7 +15,8 @@ function wf=whittaker(fnc,x0,er)
     	lf=double(f*ddf/df^2);
     	xn=double(x0-f*(2-lf)/(2*df));
     	ab=double(abs(xn-x0)/abs(xn));
-    	c=c+1
+    	c=c+1;
     end
+    c
  	wf=xn
 end
