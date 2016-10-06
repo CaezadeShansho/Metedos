8feca1775671f6b8006cdbbd86faae1c6c488b51
Thomas Munoz <thomas.fmunoz@gmail.com>
2016-10-06 18:15:38 -0300

    Update horner.m

--- Matlab/Funciones/horner.m
+++ Matlab/Funciones/horner.m
@@ -1,10 +1,10 @@
-function ans=horner(n,coeff,x0)
-	y=coeff(1)
-	z=coeff(1)
-	for i from 2 to n-1 do
+function [y,z]=horner(n,coeff,x0)
+	y=coeff(1);
+	z=coeff(1);
+    for i = 2:n-1
 		y=x0*y+coeff(i);
 		z=x0*z+y;
-	end_for
+    end
 	y=x0*y+coeff(n)
-	return (y,z)
+	z;
 end
