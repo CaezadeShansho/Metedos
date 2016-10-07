77a891162267ff3dd2cdd61f0d8b7be270d46e35
Thomas Munoz <thomas.fmunoz@gmail.com>
2016-10-06 22:29:02 -0300

    add comments

--- Matlab/Funciones/horner.m
+++ Matlab/Funciones/horner.m
@@ -1,3 +1,15 @@
+% Parámetros de Entrada :
+%
+% n : Grado del polinomio.
+% coeff : Vector que contiene todas las constantes que acompañan a x.
+% x0 : Punto a ser evaluado en la función.
+%
+% Variables de Salida:
+%
+% y : Punto x0 evaluado en f(x), es decir f(x0).
+% z : Punto x0 evaluado en f'(x), es decir f'(x0)
+
+
 function [y,z]=horner(n,coeff,x0)
 	y=coeff(1);
 	z=coeff(1);
@@ -5,6 +17,6 @@ function [y,z]=horner(n,coeff,x0)
 		y=x0*y+coeff(i);
 		z=x0*z+y;
     end
-	y=x0*y+coeff(n)
+	y=x0*y+coeff(n);
 	z;
 end
